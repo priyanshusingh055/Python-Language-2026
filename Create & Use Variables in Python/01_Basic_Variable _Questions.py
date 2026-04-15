f9139d6558e685b4aa3c3ecd19d9bd08c29ae17a
priyanshusingh055 <hellojesse999@gmail.com>
2026-04-15 12:11:37 +0530

    Basic_Variable _Questions

---
+++ Create & Use Variables in Python/01_Basic_Variable _Questions.py
@@ -0,0 +1,30 @@
+#1 Store your age in a variable and print:
+
+age = int(input("Enter Your Age :"))
+print(f"Your Age Is {age}")
+
+
+#2 Create two variables a = 10, b = 5. Print their sum.
+
+A = 10
+B = 5 
+
+print(f"Sum A And B {A+B}")
+
+
+#3 Store a float number in a variable (like price = 99.99) and print its type.
+
+Num = float(input("Enter A Float Number :"))
+print(Num)
+
+
+#4 Create a variable is_student and assign True. Print it.
+
+is_student = True
+print(is_student)
+
+
+
+
+
+
--- Create & Use Variables in Python/main.py
+++ Create & Use Variables in Python/main.py
@@ -1,3 +1,4 @@
 name = input("Enter Name :")
 
 print(f"Your Name Is {name} ")
+print(id(name))
