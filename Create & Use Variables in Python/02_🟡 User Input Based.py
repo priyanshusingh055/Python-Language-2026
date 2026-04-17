5fdc9ec1a49465021fdd69fab46ee0a128f4f9a6
priyanshusingh055 <hellojesse999@gmail.com>
2026-04-17 20:49:38 +0530

    Variable

---
+++ Create & Use Variables in Python/02_🟡 User Input Based.py
@@ -0,0 +1,13 @@
+
+# 1 Take user input for name and print:
+
+name = input("Enter Name :")
+print(f"Hello {name}")
+
+# 2 Take two numbers from user and store in variables. Print their sum.
+
+num1 = int(input("Enter Number :"))
+num2 = int(input("Enter Number :"))
+
+print(f"Sum of Two Number = {num1+num2}")
+
---
+++ Create & Use Variables in Python/03_🟠 Variable Manipulation.py
@@ -0,0 +1,18 @@
+# 1 Swap two variables:
+
+# a =10
+# b = 5
+
+# c = a 
+# a =b
+# b = c
+# print(f"A {a} and B {b}")
+
+#2 Store your first name and last name in two variables and combine them.
+
+name = "Priyansh"
+last_name = "Singh"
+
+print(name +" "+ last_name)
+
+
